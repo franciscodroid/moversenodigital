40b7bfbb31f0a59177ee7099cdd1313d11726f5f
jose francisco <144329686+franciscodroid@users.noreply.github.com>
2024-09-28 14:45:54 -0300

    Add Files

---
+++ main.js
@@ -0,0 +1,8 @@
+
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    document.querySelector('a[href="#quem-somos"]').addEventListener('click', function(e) {
+        e.preventDefault();
+        document.getElementById('quem-somos').classList.toggle('hidden');
+});
+
+
